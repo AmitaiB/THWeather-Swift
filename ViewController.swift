e8cd49c469b7f5d2537c106c134462681068ef96
AmitaiB <blickstein@gmail.com>
2015-12-13 20:49:51 -0500

    Adds a funFact label

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactProperty: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactProperty.text = "An Interesting Fact"
+
     }
 
     override func didReceiveMemoryWarning() {
